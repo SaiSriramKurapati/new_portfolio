7c6d2b9dedfc2f10ffafedbf8f4ded82ca2f0ce4
sriram <saisriramkurapati@gmail.com>
2025-11-12 17:44:16 -0500

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/new_portfolio' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/new_portfolio' : '',
 }
 
 module.exports = nextConfig
